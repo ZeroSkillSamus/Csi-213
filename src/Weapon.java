94f00d7ea99b8a34266e545d9778b78a263cf648
ZeroSkillSamus <amitchell2@albany.edu>
2017-02-11 19:06:39 -0500

    Weapons Class

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,4 +1,12 @@
 
 public class Weapon {
+	public int Damage;
 
+	public Weapon(int weight, String name, int Quanity, int Damage) {
+
+		super(weight, name, Quanity);
+		this.Damage = Damage;
+
+	}
+	
 }
