2f133be90ce755a78242ca1b63a4b46d0334c423
ZeroSkillSamus <amitchell2@albany.edu>
2017-02-12 14:04:48 -0500

    finished making the other methods and added quanity in item class instead of food

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,15 @@
 
 public class Item {
-
+	//variables names
+			private String name;
+			private int weight;
+	        private int Quanity;
+	        
+	        //constructor
+			public Item(int weight, String name, int Quanity ){
+			this.name=name;
+			this.weight=weight;
+			this.Quanity=Quanity;
+			}
+			
 }
