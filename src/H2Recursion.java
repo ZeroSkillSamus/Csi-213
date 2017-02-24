ea0e4db91b529e7d5bc75d0230082a91f1e6bfe9
ZeroSkillSamus <amitchell2@albany.edu>
2017-02-23 19:44:06 -0500

    added the base case and added double err for error

--- src/H2Recursion.java
+++ src/H2Recursion.java
@@ -1,4 +1,15 @@
+import edu.princeton.cs.introcs.StdDraw;
+
 
 public class H2Recursion {
-
+public static double babyMethod(int guess,int x,int aError){
+	
+	double e = Math.abs(arg0)
+}
+public static void main (String[]args){
+	char c = 0;
+	char a  = 1;
+	
+	System.out.println(c<a);
+}
 }
