0f0c36a53d8833b2836748657d9c5003413cf19b
ZeroSkillSamus <amitchell2@albany.edu>
2017-02-26 18:28:32 -0500

    added some test code in the main method to make sure it works

--- src/H2Recursion.java
+++ src/H2Recursion.java
@@ -2,20 +2,30 @@ import edu.princeton.cs.introcs.StdDraw;
 
 
 public class H2Recursion {
-public static double babyMethod(int guess,int x,int aError){
+public static double babyMethod(double newGuess2,int x,int aError){
 	//founds out the error and used absolute to make sure it it was positive
-	double err = Math.abs(x-guess*guess);
+	double err = Math.abs(x-newGuess2*newGuess2);
 	
 	if (x<0){
 		return 1;
 	} 
 	
 	if (err<=aError){
-		return (guess);
+		return (newGuess2);
 	}
 	
 	else 
 	{
+		double newGuess=.5*(newGuess2+x/newGuess2);
+		//System.out.println(newGuess);
+        return babyMethod(newGuess,x,aError);
+       
+	//newguess = .5 * (lastguess + x/ last guess)
+	}
+	
+	
+}
+	
 }
 
-}
+
