7497f9c2929042dda42ed5ac458b15cee7409c59
ZeroSkillSamus <amitchell2@albany.edu>
2017-02-26 18:27:36 -0500

    Added the new guess and added the return statement to recursively call the method

--- src/H2Recursion.java
+++ src/H2Recursion.java
@@ -3,12 +3,19 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class H2Recursion {
 public static double babyMethod(int guess,int x,int aError){
-	
+	//founds out the error and used absolute to make sure it it was positive
 	double err = Math.abs(x-guess*guess);
 	
 	if (x<0){
-		return 1
+		return 1;
 	} 
+	
+	if (err<=aError){
+		return (guess);
+	}
+	
+	else 
+	{
 }
 
 }
