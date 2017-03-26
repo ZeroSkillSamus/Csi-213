1ef318a81357b4aefe0f25fefe6f009c483620ed
ZeroSkillSamus <amitchell2@albany.edu>
2017-03-26 19:58:55 -0400

    added the add class with the first 2 cases

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -1,4 +1,26 @@
 
 public class DoublyLinkedList {
+	public Node root;
+	public Node tail;
+	public float[] thingag;
+	/*
+	 * method to keep track of the size of the list
+	 * traverses the list and increments the counter every time something is added
+	 * returns the counter 
+	 */
+	public int size()
+	{
+		
+		int counter = 0;
+		Node temp = root;
+		
+		while(temp != null)
+		{
+			counter++;
+			temp = temp.getNext();
+		}
+		
+		return counter;
+	}
 
 }
