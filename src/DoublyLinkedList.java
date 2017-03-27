82ce1784f5074212b134f66bff8614ab2a780e51
ZeroSkillSamus <amitchell2@albany.edu>
2017-03-26 20:09:58 -0400

    added the destroy list method

--- src/DoublyLinkedList.java
+++ src/DoublyLinkedList.java
@@ -22,5 +22,36 @@ public class DoublyLinkedList {
 		
 		return counter;
 	}
-
+	public void add(String n){
+		
+        Node newNode = new Node(n);
+        
+       //if there is nothing in the list
+        if (root == null) {
+        root = tail = newNode;
+    }
+        //checks to see if the list has 1 item in it
+	else if(size()==1)
+	{
+		/*compares the newnode data to the tails data 
+		 * if its greater than 0 add after the new node after the list
+		 * makes the newnode the tail
+		 */
+		if(newNode.getName().compareToIgnoreCase(tail.getName())>0)
+		{
+			tail = newNode;
+			root.setNext(tail);
+			tail.setPrev(root);
+		}
+		/*
+		 * will add before the list if the first if statement is false
+		 * makes the newnode the new root
+		 */
+		else
+		{
+			root = newNode;
+			root.setNext(tail);
+			tail.setPrev(root);
+		}
+	}
 }
