fdbd7688860bd4a9d756a8e02eeb93a37714a9aa
ZeroSkillSamus <amitchell2@albany.edu>
2017-04-09 16:01:16 -0400

    added the print queue method

--- src/Queues.java
+++ src/Queues.java
@@ -5,18 +5,38 @@ public int size;
 
 
 //checks the size and returns the size of the Queues
-public int size(){
+public int size()
+{
 	int counter = 0;
 	Node temp = head;
-	while (temp!=null){
+	while (temp!=null)
+	{
+		
 		counter++;
 		temp = temp.getNext();
+		
 	}
 	return counter;
 	}
 
-public boolean isEmpty(){
+//checks to see if the list is empty
+public boolean isEmpty()
+{
+    //in order to check it checks to see if the head is equal to null
 	return (head == null);
+}
+
+public void printQueue ()
+{
+	Node temp = head;
+	
+	while (temp!=null)
+	{
+		System.out.println(temp.getName());
+		temp = temp.getNext();
+	}
+	
+	
 }
 public void enqueue (Node n){
 	
