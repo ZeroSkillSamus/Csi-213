35799cf8c5ff9406e5306956988c0dc44149b981
ZeroSkillSamus <amitchell2@albany.edu>
2017-04-09 16:34:22 -0400

    added the size method for stack class

--- src/Stack.java
+++ src/Stack.java
@@ -1,16 +1,88 @@
 
-public class Stack {
-public Node node;
 
-public void push (Node n){
+
+public class Stack {
+public static Node head;
+
+public static void push (String n){
+	Node newNode = new Node(n);
+	if (head==null)
+	{
+	      head = newNode;
+	      //System.out.println(head);
+		
+	} 
+	else 
+	{
+		//System.out.println(head);
+		Node temp = head;
+		while(temp!=null)
+		{
+			temp = temp.getNext();
+			
+			System.out.println(newNode);
+		
+			
+		}
+		
+		temp = newNode;
+		//System.out.println(n);
+		
+	}
+	
+	//System.out.println(newNode);
 	
 }
 
-public Node pop (Node n){
-	return n;
+public static void pop ()
+{
+	if (head==null){
+		return;
+	} 
+	else
+	{
+	
+		Node temp = head;
+		while(temp!=null)
+		{
+			System.out.println(temp.getPrev());
+			temp = temp.getNext();
+			
+			temp = null;
+		}
+		System.out.println();
+		//temp = null;
+		
+	}
+	
 }
 
-public Node peek (){
-	return node;
+public static Node peek (){
+	if (head==null)
+		{
+		return null;
+		}
+	else {
+		Node temp = head;
+		while(temp!=null){
+			System.out.println(temp);
+			temp = temp.getNext();
+	
+		}
+	return temp;
+	}
+}
+
+public static void main (String[]args){
+	Node n = new Node("String");
+	push("I");
+	push("Love");
+	push("Computer");
+	push("Science");
+	
+	
+	pop();
+
+	
 }
 }
