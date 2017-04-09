2c05d9a0e3f1259b7b9bc6a8819001597e648397
ZeroSkillSamus <amitchell2@albany.edu>
2017-04-09 16:46:08 -0400

    made a stack to queue method

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,61 @@ public class Main {
 		newQueue.printQueue();
 		}
 	
+	//turns the stack to a queue
+public static Queues stackstoQueues(Stack stack){
+		//makes a new queue
+		Queues queues = new Queues(); 
+		
+		//stores the stack.pop into a temp
+		String temp = stack.pop();
+		
+		//while the list is not empty
+		while(temp != null)
+		{
+			//enqueue the stuff that you poped into the new queue
+			queues.enqueue(temp);
+			//make sure the temp decrements until its empty
+			temp = stack.pop();
+		}
+	    
+	    return queues;
+		}
+
+	/*public static Stack queuestoStacks(Queues queue)
+	{
+		Stack stack = new Stack();
+		String temp = queue.dequeue();
+		
+		while (temp != null) {
+			stack.push(temp);
+			temp = queue.dequeue();
+		}
+		
+		return stack;
+		/*int length = queue.size();
+		
+		for ( int i = 0;i<length;i++){
+			stack.push(queue.dequeue());
+		}
+		
+		return stack;
+		*/
 	
+	
+	public static Stack stackstoStacks(Stack stack)
+	{
+		Stack stack1 = new Stack();
+		Stack stack2 = new Stack();
+		
+		int length = stack.size();
+		for (int i = 0;i<length;i++){
+		stack1.push(stack.pop());
+		}
+		for(int j = 0; j < length; j++){
+			stack2.push(stack1.pop());
+		}
+		
+		return stack2;
+	}
 
 	}
