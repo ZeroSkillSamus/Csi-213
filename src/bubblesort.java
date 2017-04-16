315d2f8ccec0f598d1f755693ae4b96222a773c3
ZeroSkillSamus <amitchell2@albany.edu>
2017-04-16 17:46:05 -0400

    added the size method

--- src/bubblesort.java
+++ src/bubblesort.java
@@ -1,4 +1,18 @@
 
 public class bubblesort {
+public Node head;
 
+public int size (){
+	if (head == null){
+		return (Integer) null;
+	} else {
+		Node temp = head;
+		int counter = 0;
+		while (temp!=null){
+			counter++;
+			temp = temp.getNext();
+		}
+		return counter;
+	}
+}
 }
