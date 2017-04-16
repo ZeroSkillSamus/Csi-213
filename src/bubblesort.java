e44fd479c9a1b33d7696e1db5c1d2a96cee40e55
ZeroSkillSamus <amitchell2@albany.edu>
2017-04-16 17:50:54 -0400

    added the add node method

--- src/bubblesort.java
+++ src/bubblesort.java
@@ -2,17 +2,32 @@
 public class bubblesort {
 public Node head;
 
+public boolean isEmpty(){
+	return (head==null);
+}
 public int size (){
-	if (head == null){
-		return (Integer) null;
+	int counter = 0;
+	if (isEmpty()){
+		return counter;
 	} else {
 		Node temp = head;
-		int counter = 0;
 		while (temp!=null){
 			counter++;
 			temp = temp.getNext();
 		}
 		return counter;
+	}
+}
+public void add (int n){
+	Node newNode = new Node (n);
+	if (isEmpty()){
+		head = newNode;
+	} else {
+		Node temp = head;
+		while (temp!=null){
+			temp = temp.getNext();
+		}
+		temp.setNext(newNode);
 	}
 }
 }
