512376c2734e87d49b6ce2d3d0e005d485bfd1cf
ZeroSkillSamus <amitchell2@albany.edu>
2017-05-07 23:18:11 -0400

    added the add part 2 method

--- src/BST.java
+++ src/BST.java
@@ -48,6 +48,47 @@ public void inorder() {
 				add(temp, d);
 			}
 		}
+		/*
+		 * Adds the nodes in by using recursion 
+		 * has 3 cases 
+		 */
+		private void add(BSTNode temp, String d)
+		{
+			
+	     //checks to see if what your adding is less than 0
+	     //add it to the left side
+			if ((d.compareToIgnoreCase(temp.getName())) < 0)
+			{
+
+				if (temp.getLeft() != null) {
+
+					add(temp.getLeft(), d);
+				} else {
+
+					temp.setLeft(new BSTNode(d, 1));
+					temp.getLeft().setParent(temp);
+				}
+			}
+	         //checks to see what your adding is greater than zero 
+			//add it to the right side
+			else if ((d.compareToIgnoreCase(temp.getName())) > 0) {
+
+				if (temp.getRight() != null) {
+					add(temp.getRight(), d);
+				}
+
+				else {
+					temp.setRight(new BSTNode(d, 1));
+					temp.getRight().setParent(temp);
+				}
+			}
+	//checks to see what your adding is equal to the head 
+	//if it is increment the counter by 1
+			else if (d.compareTo(temp.getName()) == 0) {
+				temp.incrementCounter();
+			}
+		}
+
 
 }
 
