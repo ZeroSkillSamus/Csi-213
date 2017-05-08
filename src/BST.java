d35802c20215aa97d38b3047cf8e467b328016f2
ZeroSkillSamus <amitchell2@albany.edu>
2017-05-07 23:01:28 -0400

    added the next part of the inorder method

--- src/BST.java
+++ src/BST.java
@@ -1,6 +1,5 @@
 
 public class BST {
-public BSTNode root;
 
 public BSTNode head;
 
@@ -8,6 +7,15 @@ public BSTNode head;
 public void inorder() {
 	inorder(head);
 }
+//does the other stuff for inorder method by using recurssion
+	private void inorder(BSTNode r) {
+		if (r != null) {
+			inorder(r.getLeft());
+			System.out.println(r);
+			inorder(r.getRight());
+
+		}
+	}
 
 }
 
